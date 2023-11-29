e948fe420bc4009822e969a8c3eaf7ee28e314ce
ChanukyaVardhan <chanukyagujjula@gmail.com>
2023-11-29 14:43:41 -0500

    add simple retrieval endpoint as well

--- gcp/python/text_embedding_server.py
+++ gcp/python/text_embedding_server.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from concurrent import futures
 from google.cloud.sql.connector import Connector
 from pgvector.asyncpg import register_vector
@@ -44,6 +45,7 @@ class TextEmbeddingServicer(text_embedding_pb2_grpc.TextEmbedding):
 
         try:
             # CHECK - FIX THIS, IS IT BETTER TO MAINTAIN A POOL?
+            # CHECK - INSERT CHUNKS AS A BATCH INSTEAD OF ONE EACH TIME
             for chunk_number, (chunk, embedding) in enumerate(zip(chunks, all_embeddings)):
                 await self.db_pool.execute(
                         "INSERT INTO text_embeddings (url, chunk_number, text, embedding) VALUES ($1, $2, $3, $4)",
@@ -56,6 +58,58 @@ class TextEmbeddingServicer(text_embedding_pb2_grpc.TextEmbedding):
             status = False
 
         return text_embedding_pb2.SaveTextEmbeddingResponse(status = status)
+
+    async def GetPreferenceArticles(self, request, context):
+        logging.info(f"Start GetPreferenceArticles request")
+        # CHECK - LIMIT TEXT TO 512 TOKENS? - OR JUST ERROR
+        preference_text = request.preference_text
+
+        _, preference_embedding = self.bert.get_embedding(preference_text, split_chunks = False)
+
+        similarity_threshold = 0.9
+        num_matches = 10
+        try:
+            # CHECK - FIX THE QUERY BASED ON REQUIREMENT. SHOULD BE DISTINCT URLs SEARCH?
+            query_results = await self.db_pool.fetch(
+                """
+                WITH vector_matches AS (
+                  SELECT url, 1 - (embedding <=> $1) AS similarity
+                  FROM text_embeddings
+                  WHERE 1 - (embedding <=> $1) > $2
+                  ORDER BY similarity DESC
+                  LIMIT $3
+                )
+                SELECT url, chunk_number, text FROM text_embeddings
+                WHERE url IN (SELECT url FROM vector_matches)
+                """,
+                preference_embedding,
+                similarity_threshold,
+                num_matches
+            )
+
+            # Process the query results
+            articles_by_url = defaultdict(list)
+            for row in query_results:
+                url, chunk_number, text = row['url'], row['chunk_number'], row['text']
+                articles_by_url[url].append((chunk_number, text))
+
+            # Construct the response using Proto message types
+            response = text_embedding_pb2.GetPreferenceArticlesResponse()
+            # CHECK - WILL THIS CHANGE THE ORDER?? - DON'T THINK SO
+            for url, chunks in articles_by_url.items():
+                sorted_chunks = sorted(chunks, key=lambda x: x[0])
+                article_text = ' '.join([chunk[1] for chunk in sorted_chunks])
+                article = text_embedding_pb2.PreferenceArticle(url=url, summary=article_text)
+                response.article.append(article)
+
+            logging.info(f"Completed GetPreferenceArticles request")
+            return response
+
+        except Exception as e:
+            logging.error(f"Error retrieving: {e}")
+
+        return text_embedding_pb2.GetPreferenceArticlesResponse()
+
 
 
 async def serve():
